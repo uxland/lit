575f20a0a27fdd48bd6f2c7023cc033c65d17497
Alex Vizcaino <avizcaino@uxland.es>
2021-06-22 13:16:11 +0200

    fix(lit-mixins): localization mixin

---
+++ packages/lit-mixins/locale-mixin.ts
@@ -0,0 +1,22 @@
+import {
+  localeMixin,
+  LocalizationMixin,
+  localizerFactory,
+  LocalizerFactory,
+} from '@uxland/localization';
+import {dedupeMixin} from '@uxland/utilities/dedupe-mixin';
+import {LitElement} from 'lit';
+
+export interface LocalizationMixinConstructor extends LitElement {
+  new (...args: any[]): LocalizationMixin & LitElement;
+}
+type Constructor<T = Record<string, unknown>> = new (...args: any[]) => T;
+export type LitLocaleMixinFunction = (
+  superClass: Constructor<LitElement>
+) => LocalizationMixinConstructor;
+
+export function litLocaleMixin(factory: LocalizerFactory): LitLocaleMixinFunction {
+  return dedupeMixin((superClass: Constructor<LitElement>) => localeMixin(factory)(superClass));
+}
+
+export const litLocale = litLocaleMixin(localizerFactory);
