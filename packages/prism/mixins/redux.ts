07abf816c528e2b7fd9fbc6319ddcf49f2318b48
Alex Vizcaino <avizcaino@uxland.es>
2021-07-30 16:23:18 +0200

    fix(prism): update redux

--- packages/prism/mixins/redux.ts
+++ packages/prism/mixins/redux.ts
@@ -1,4 +1,4 @@
 import {connect, ConnectMixinFunction} from '@uxland/lit-redux-connect/connect';
 import {store} from '../store';
 
-export const redux: ConnectMixinFunction = connect(<any>store);
+export const redux: ConnectMixinFunction = connect(store);
