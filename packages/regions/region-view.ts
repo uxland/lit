221e0e66b94b0f4e85e8e937ea864065dcc78687
Alex Vizcaino <avizcaino@uxland.es>
2021-07-30 11:01:04 +0200

    fix(regions): change regionView mixin typing

--- packages/regions/region-view.ts
+++ packages/regions/region-view.ts
@@ -1,11 +1,9 @@
-import {dedupeMixin, propertiesObserver} from '@uxland/lit-utilities';
+import {propertiesObserver} from '@uxland/lit-utilities';
 import {LitElement} from 'lit';
 import {property} from 'lit/decorators.js';
 import {ViewDefinition} from './view-definition';
 
-export type RegionViewMixinConstructor = RegionViewMixin & typeof LitElement;
-
-export type RegionViewMixinFunction = (superClass: typeof LitElement) => RegionViewMixinConstructor;
+type Constructor<T = Record<string, unknown>> = new (...args: any[]) => T;
 
 export interface RegionView extends LitElement {
   active: boolean;
@@ -17,19 +15,15 @@ export interface RegionView extends LitElement {
 
 export interface RegionViewMixin extends RegionView {}
 
-export function regionViewMixin(): RegionViewMixinFunction {
-  return dedupeMixin((superClass: typeof LitElement) => {
-    class RegionView extends propertiesObserver(superClass) implements RegionViewMixin {
-      @property({type: Boolean})
-      active: boolean;
-      activeChanged(current: boolean, previous: boolean) {}
-      view: ViewDefinition;
-      @property()
-      regionContext: any;
-      regionContextChanged(newContext: any, oldContext: any) {}
-    }
-    return RegionView;
-  });
-}
-
-export const regionView = regionViewMixin();
+export const regionView = <T extends Constructor<LitElement>>(superClass: T) => {
+  class RegionView extends propertiesObserver(superClass) implements RegionViewMixin {
+    @property({type: Boolean})
+    active: boolean;
+    activeChanged(current: boolean, previous: boolean) {}
+    view: ViewDefinition;
+    @property()
+    regionContext: any;
+    regionContextChanged(newContext: any, oldContext: any) {}
+  }
+  return RegionView as Constructor<RegionViewMixin> & T;
+};
