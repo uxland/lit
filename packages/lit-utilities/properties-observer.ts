c2629eb87d13b116353283938f43105783259701
Alex Vizcaino <avizcainoluna@gmail.com>
2023-05-17 14:28:19 +0200

    fix: dedupe propertiesObserver mixin

--- packages/lit-utilities/properties-observer.ts
+++ packages/lit-utilities/properties-observer.ts
@@ -2,6 +2,7 @@ import {Constructor} from '@uxland/utilities/dedupe-mixin';
 import {LitElement, notEqual} from 'lit';
 import defaultTo from 'ramda/es/defaultTo';
 import pipe from 'ramda/es/pipe';
+import {dedupeMixin} from './dedupe-mixin';
 
 export interface PropertiesObserverMixin {}
 
@@ -11,29 +12,29 @@ const getPropertyComponentComparer = (name: PropertyKey, component: any) =>
     : undefined;
 const comparer = pipe(getPropertyComponentComparer, defaultTo(notEqual));
 
-export const propertiesObserver = <T extends Constructor<LitElement>>(
-  superClass: T
-): T & Constructor<PropertiesObserverMixin> => {
-  class PropertiesObserver extends superClass {
-    updated(changedProperties) {
-      super.updated(changedProperties);
-      changedProperties.forEach((oldValue, propName) => {
-        if (comparer(propName, this)(this[propName], oldValue)) {
-          if (this[`${String(propName)}Changed`])
-            this[`${String(propName)}Changed`](this[propName], oldValue);
-        }
-      });
-    }
-    requestUpdate(name, oldValue) {
-      const result = super.requestUpdate
-        ? super.requestUpdate(name, oldValue)
-        : Promise.resolve(null);
-      if (comparer(name, this)(this[name], oldValue)) {
-        if (this[`${String(name)}Changed`]) this[`${String(name)}Changed`](this[name], oldValue);
+export const propertiesObserver = dedupeMixin(
+  <T extends Constructor<LitElement>>(superClass: T): T & Constructor<PropertiesObserverMixin> => {
+    class PropertiesObserver extends superClass {
+      updated(changedProperties) {
+        super.updated(changedProperties);
+        changedProperties.forEach((oldValue, propName) => {
+          if (comparer(propName, this)(this[propName], oldValue)) {
+            if (this[`${String(propName)}Changed`])
+              this[`${String(propName)}Changed`](this[propName], oldValue);
+          }
+        });
       }
+      requestUpdate(name, oldValue) {
+        const result = super.requestUpdate
+          ? super.requestUpdate(name, oldValue)
+          : Promise.resolve(null);
+        if (comparer(name, this)(this[name], oldValue)) {
+          if (this[`${String(name)}Changed`]) this[`${String(name)}Changed`](this[name], oldValue);
+        }
 
-      return result;
+        return result;
+      }
     }
+    return PropertiesObserver;
   }
-  return PropertiesObserver;
-};
+);
