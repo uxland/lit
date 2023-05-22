897fb440c937c041fd3c1aa09856bcfd16e4b2ea
Alex Vizcaino <avizcainoluna@gmail.com>
2023-05-22 22:05:01 +0200

    fix: correct redux mixin with new typings

--- packages/prism/mixins/redux.ts
+++ packages/prism/mixins/redux.ts
@@ -1,4 +1,8 @@
-import {connect, ConnectMixinFunction} from '@uxland/lit-redux-connect/connect';
+import {connect, ConnectMixin} from '@uxland/lit-redux-connect/connect';
+import {Constructor} from '@uxland/utilities';
+import {LitElement} from 'lit';
 import {store} from '../store';
 
-export const redux: ConnectMixinFunction = connect(store);
+export function redux<T extends Constructor<LitElement>>(superClass: T) {
+  return connect(store)(superClass) as Constructor<ConnectMixin> & T;
+}
